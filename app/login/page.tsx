c101629b79e1697d9c7aa91b15cbc800abc99580
Alperen Özkan <alperenweb3@gmail.com>
2024-06-06 11:23:29 +0200

    chore: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+import CodeWorkersLogo from '@/app/ui/codeworkers-logo';
+import LoginForm from '@/app/ui/login-form';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-white p-3 ml:h-36">
+          <div className="w-32 text-white md:w-36">
+            <CodeWorkersLogo color='text-black' />
+          </div>
+        </div>
+        <LoginForm />
+        <Link className='text-[10px] text-right hover:text-orange-600' href="/">Back to Home</Link>
+      </div>
+    </main>
+  );
+}
--- app/ui/codeworkers-logo.tsx
+++ app/ui/codeworkers-logo.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-export default function CodeWorkersLogo() {
+export default function CodeWorkersLogo({color}: {color: string}) {
   return (
     <div
       className={`flex flex-row items-center leading-none`}
     >
       <Image src="/icon.svg" alt="CodeWorkers Logo" width={55} height={0} />
-      <p className="text-2xl">CodeWorkers</p>
+      <p className={`text-2xl ${color}`}>CodeWorkers</p>
     </div>
   );
 }
