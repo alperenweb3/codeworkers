de43d92e9069461059b8c03482c712b3836e40d9
Alperen Özkan <alperenweb3@gmail.com>
2024-06-11 16:36:30 +0200

    style: layout fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   // });
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="w-full flex-none md:w-72">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,7 +7,7 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-gray-50 p-4 md:h-40"
+        className="mb-2 flex h-20 items-center justify-start rounded-md bg-gray-50 p-2 md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
